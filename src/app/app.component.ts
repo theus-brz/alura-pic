6a3cf904bb8984f79dc61182b6f05eaac452332d
Matheus Barbosa <matheusbrbs@gmail.com>
2020-11-25 14:03:44 -0300

    adding API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PhotoService} from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url:
-        'https://exitoina.uol.com.br/media/_versions/lion-3372731_1920_widelg.jpg',
-      description: 'Leão',
-    },
-    {
-      url:
-        'https://s2.glbimg.com/awP2KnJnzIYEWAVOYLFL4d2nG0I=/0x0:2400x1599/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/a/m/0T0EZ4QZqcrtzp9ZwNeg/2018-10-21t152242z-534837741-rc174f509210-rtrmadp-3-usa-zoo-lions.jpg',
-      description: 'Leoa',
-    },
-  ];
+  photos: Object[] = [];
+
+  constructor(photoService: PhotoService) {
+    photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
+  }
 }
