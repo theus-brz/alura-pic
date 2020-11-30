7b99dcabaa1eba4eccd52399c3c17ad4699513bb
Matheus Barbosa <matheusbrbs@gmail.com>
2020-11-30 15:59:58 -0300

    routing application

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,9 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
   templateUrl: 'photo.component.html',
-
 })
 export class PhotoComponent {
   @Input()
