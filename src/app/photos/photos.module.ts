236e3d10ee02a564c3d599e8aef4c0d2b4a21d7c
Matheus Barbosa <matheusbrbs@gmail.com>
2020-12-01 18:07:42 -0300

    better user experience

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,6 +6,8 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { PhotosComponent } from './photo-list/photos/photos.component';
     PhotoListComponent,
     PhotoFormComponent,
     PhotosComponent,
+    FilterByDescription,
+    LoadButtonComponent,
   ],
   imports: [
     HttpClientModule,
