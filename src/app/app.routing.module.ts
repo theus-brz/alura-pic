1d25eb30d075b8bd504e378d428144bd1c31bfaa
Matheus Barbosa <matheusbrbs@gmail.com>
2020-12-08 10:55:39 -0300

    feat: modules and imports from sign in page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,9 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
-import {PhotoListResolver} from './photos/photo-list/photo-list.resolver';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { SignInComponent } from './home/signin/signin.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent
+  },
   {
     path: 'user/:userName',
     component: PhotoListComponent,
