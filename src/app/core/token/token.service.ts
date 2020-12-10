ce3dea3a73e8c45b002fd856e7735f369f7d03b5
Matheus Barbosa <matheusbrbs@gmail.com>
2020-12-10 20:11:42 -0300

    feat: token service

---
+++ src/app/core/token/token.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'authToken';
+
+@Injectable({ providedIn: 'root' })
+export class TokenService {
+  hasToken(): boolean {
+    return !!this.getToken();
+  }
+
+  setToken(token): void {
+    window.localStorage.setItem(KEY, token);
+  }
+
+  getToken(): string {
+    return window.localStorage.getItem(KEY);
+  }
+
+  removeToken(): void {
+    window.localStorage.removeItem(KEY);
+  }
+}
