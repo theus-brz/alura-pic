5d6b023d20d66abb26bc0ae6766c41eb2134674c
Matheus Barbosa <matheusbrbs@gmail.com>
2020-12-15 23:27:54 -0300

    feat: header:

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  user$: Observable<User>;
+  user: User;
+
+  constructor(private userService: UserService) {
+    this.user$ = userService.getUser();
+    this.user$.subscribe(user => this.user = user);
+  }
+}
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from './user';
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(null);
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.hasToken()) {
+      this.decodeAndNotify();
+    }
+  }
+
+  setToken(token: string): void {
+    this.tokenService.setToken(token);
+    this.decodeAndNotify();
+  }
+
+  getUser(): Observable<User> {
+    return this.userSubject.asObservable();
+  }
+
+  private decodeAndNotify(): void {
+    const token = this.tokenService.getToken();
+    const user = jwt_decode(token) as User;
+    this.userSubject.next(user);
+  }
+}
