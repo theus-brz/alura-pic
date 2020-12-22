297987d730cbaabbf339a6162b3e9481311633e4
Matheus Barbosa <matheusbrbs@gmail.com>
2020-12-22 18:25:57 -0300

    feat: route guard

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -7,6 +7,7 @@ import * as jwt_decode from 'jwt-decode';
 @Injectable({providedIn: 'root'})
 export class UserService {
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
     if (this.tokenService.hasToken()) {
@@ -26,11 +27,20 @@ export class UserService {
   private decodeAndNotify(): void {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
   }
 
-  logout() {
+  logout(): void {
     this.tokenService.removeToken();
     this.userSubject.next(null);
+  }
+
+  isLogged(): boolean {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName(): string {
+    return this.userName;
   }
 }
