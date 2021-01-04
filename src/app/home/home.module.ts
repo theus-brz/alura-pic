f31b2e5818c2e7293e782674ae0d544fb701c6e6
Matheus Barbosa <matheusbrbs@gmail.com>
2021-01-04 09:35:23 -0300

    feat: signup component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [ SignInComponent ],
+  declarations: [
+    SignInComponent,
+    SignUpComponent,
+  ],
   imports: [
-    ReactiveFormsModule,
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     VMessageModule,
     RouterModule,
   ]
