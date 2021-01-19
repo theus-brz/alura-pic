4c91e7dcf09c694e8991779b0d9437bc3ec1cd71
Matheus Barbosa <matheusbrbs@gmail.com>
2021-01-19 14:16:12 -0300

    feat: child routes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,11 @@ import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     SignInComponent,
     SignUpComponent,
   ],
